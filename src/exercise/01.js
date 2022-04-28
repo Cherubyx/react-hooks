a6a98086a5f38f300bd3d9af25158f428cc39d25
Tiffany Ip <tiffany.ip@shopify.com>
2022-04-28 14:52:50 -0400

    Exercise 01 Complete

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,16 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
-      <form>
+      <form onSubmit={() => {}}>
         <label htmlFor="name">Name: </label>
         <input onChange={handleChange} id="name" />
       </form>
@@ -23,7 +22,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Tiff" />
 }
 
 export default App
