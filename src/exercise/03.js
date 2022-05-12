718d7ce34a2234dc6028406c32cf1cf118f66ba2
Tiffany Ip <tiffany.ip@shopify.com>
2022-05-12 10:11:12 -0400

    Exercise 03 Complete

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,51 +3,70 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+// Extra credit 1: Colocate name property use state to component level
+// function Name({name, onNameChange}) {
+//   return (
+//     <div>
+//       <label htmlFor="name">Name: </label>
+//       <input id="name" value={name} onChange={onNameChange} />
+//     </div>
+//   )
+// }
+
+function Name() {
+  const [name, setName] = React.useState('')
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
-    </div>
-  )
-}
-
-// 🐨 accept `animal` and `onAnimalChange` props to this component
-function FavoriteAnimal() {
-  // 💣 delete this, it's now managed by the App
-  const [animal, setAnimal] = React.useState('')
-  return (
-    <div>
-      <label htmlFor="animal">Favorite Animal: </label>
       <input
-        id="animal"
-        value={animal}
-        onChange={event => setAnimal(event.target.value)}
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
       />
     </div>
   )
 }
 
-// 🐨 uncomment this
+// 🐨 accept `animal` and `onAnimalChange` props to this component
+function FavoriteAnimal({animal, onAnimalChange}) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input id="animal" value={animal} onChange={onAnimalChange} />
+    </div>
+  )
+}
+
+// Extra credit 1: Remove name
 // function Display({name, animal}) {
 //   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
 // }
-
-// 💣 remove this component in favor of the new one
-function Display({name}) {
-  return <div>{`Hey ${name}, you are great!`}</div>
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   // 🐨 add a useState for the animal
-  const [name, setName] = React.useState('')
+  // Extra credit 1: Move it down to component level
+  // const [name, setName] = React.useState('')
+  const [animal, setAnimal] = React.useState('')
+
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      {/* Extra credit 1 - Move name property down into the component */}
+      {/* <Name name={name} onNameChange={event => setName(event.target.value)} /> */}
+      <Name />
+
       {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
-      <FavoriteAnimal />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={event => setAnimal(event.target.value)}
+      />
       {/* 🐨 pass the animal prop here */}
-      <Display name={name} />
+      {/* <Display name={name} animal={animal} /> */}
+
+      {/* Extra credit 1 - only showing animal now, name is to colocate */}
+      <Display animal={animal} />
     </form>
   )
 }
